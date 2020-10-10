48a5aee7311a8d3426dc78b4ca9f1678221a450e
lehuuphuc2349 <57623623+lehuuphuc2349@users.noreply.github.com>
2020-10-10 18:43:58 +0700

    add

--- src/model/Orders.java
+++ src/model/Orders.java
@@ -10,5 +10,52 @@ package model;
  * @author Phucdz
  */
 public class Orders {
+	private int id;
+	private int drinksID;
+	private int invoiceID;
+	private int count;
+
+	public Orders() {
+	}
+
+	public Orders(int id, int drinksID, int invoiceID, int count) {
+		this.id = id;
+		this.drinksID = drinksID;
+		this.invoiceID = invoiceID;
+		this.count = count;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getDrinksID() {
+		return drinksID;
+	}
+
+	public void setDrinksID(int drinksID) {
+		this.drinksID = drinksID;
+	}
+
+	public int getInvoiceID() {
+		return invoiceID;
+	}
+
+	public void setInvoiceID(int invoiceID) {
+		this.invoiceID = invoiceID;
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+	
 	
 }
--- src/model/Tables.java
+++ src/model/Tables.java
@@ -10,5 +10,52 @@ package model;
  * @author Phucdz
  */
 public class Tables {
+	private int tableID;
+	private String tableName;
+	private String tableNote;
+	private String tableStatus;
+
+	public Tables() {
+	}
+
+	public Tables(int tableID, String tableName, String tableNote, String tableStatus) {
+		this.tableID = tableID;
+		this.tableName = tableName;
+		this.tableNote = tableNote;
+		this.tableStatus = tableStatus;
+	}
+
+	public int getTableID() {
+		return tableID;
+	}
+
+	public void setTableID(int tableID) {
+		this.tableID = tableID;
+	}
+
+	public String getTableName() {
+		return tableName;
+	}
+
+	public void setTableName(String tableName) {
+		this.tableName = tableName;
+	}
+
+	public String getTableNote() {
+		return tableNote;
+	}
+
+	public void setTableNote(String tableNote) {
+		this.tableNote = tableNote;
+	}
+
+	public String getTableStatus() {
+		return tableStatus;
+	}
+
+	public void setTableStatus(String tableStatus) {
+		this.tableStatus = tableStatus;
+	}
+	
 	
 }
