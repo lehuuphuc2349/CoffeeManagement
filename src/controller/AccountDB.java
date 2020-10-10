c080fc738eea6240f9e4fc2ba36fd6b5828ecbf4
lehuuphuc2349 <57623623+lehuuphuc2349@users.noreply.github.com>
2020-10-10 20:29:14 +0700

    commit

--- src/controller/AccountDB.java
+++ src/controller/AccountDB.java
@@ -5,6 +5,12 @@
  */
 package controller;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 import model.Account;
 
 /**
@@ -26,6 +32,33 @@ public class AccountDB {
 	}
 	public static void setInstance() {
 		AccountDB.instance = instance;
+	}
+	public Boolean Login(String username, String password) throws Exception{
+		Connection connection = controller.ConnectDatabase.ConnectMySQLSever();
+		String state = "Select * from Account Where username=? And password=?";
+		PreparedStatement preparedStatement = connection.prepareStatement(state);
+		preparedStatement.setString(1, username);
+		preparedStatement.setString(2, password);
+		ResultSet result = preparedStatement.executeQuery();
+		if(result.next()) {
+			return true;
+		} else return false;
+	} 
+	public Account GetAccount() {
+		return account;
+	}
+	
+	public List<Account> listAccount() throws Exception {
+		List<Account> listAccount = new ArrayList<>();
+		Connection connection = controller.ConnectDatabase.ConnectMySQLSever();
+		String state = "Select * from Account";
+		Statement statement = connection.createStatement();
+		ResultSet result = statement.executeQuery(state);
+		while(result.next()) {
+			Account account = new Account(result.getInt(1) , result.getString(2) , result.getString(3) , result.getString(4));
+			listAccount.add(account);
+		}
+		return listAccount;
 	}
 	
 }
