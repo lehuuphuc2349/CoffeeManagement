153f1da7f0e69f5ee376c9d61ca958859cf060f3
lehuuphuc2349 <57623623+lehuuphuc2349@users.noreply.github.com>
2020-10-10 21:01:36 +0700

    commit

--- src/controller/AccountDB.java
+++ src/controller/AccountDB.java
@@ -18,48 +18,55 @@ import model.Account;
  * @author Phucdz
  */
 public class AccountDB {
+
 	private static AccountDB instance;
 	Account account = new Account();
 
 	public AccountDB() {
 	}
-	
+
 	public static AccountDB getInstance() {
-		if(instance == null) {
+		if (instance == null) {
 			instance = new AccountDB();
 		}
 		return instance;
 	}
+
 	public static void setInstance() {
 		AccountDB.instance = instance;
 	}
-	public Boolean Login(String username, String password) throws Exception{
+
+	public Boolean Login(String username, String password) throws Exception {
 		Connection connection = controller.ConnectDatabase.ConnectMySQLSever();
 		String state = "Select * from Account Where username=? And password=?";
 		PreparedStatement preparedStatement = connection.prepareStatement(state);
 		preparedStatement.setString(1, username);
 		preparedStatement.setString(2, password);
 		ResultSet result = preparedStatement.executeQuery();
-		if(result.next()) {
+		if (result.next()) {
 			return true;
-		} else return false;
-	} 
+		} else {
+			return false;
+		}
+	}
+
 	public Account GetAccount() {
 		return account;
 	}
-	
+
 	public List<Account> listAccount() throws Exception {
 		List<Account> listAccount = new ArrayList<>();
 		Connection connection = controller.ConnectDatabase.ConnectMySQLSever();
 		String state = "Select * from Account";
 		Statement statement = connection.createStatement();
 		ResultSet result = statement.executeQuery(state);
-		while(result.next()) {
-			Account account = new Account(result.getInt(1) , result.getString(2) , result.getString(3) , result.getString(4));
+		while (result.next()) {
+			Account account = new Account(result.getInt(1), result.getString(2), result.getString(3), result.getString(4));
 			listAccount.add(account);
 		}
 		return listAccount;
 	}
+
 	public Boolean Add(String username, String password, String name) throws Exception {
 		Connection connection = controller.ConnectDatabase.ConnectMySQLSever();
 		String state = "Insert into Account(username, password, name) values(?,?,?)";
@@ -68,9 +75,51 @@ public class AccountDB {
 		preparedStatement.setString(2, password);
 		preparedStatement.setString(3, name);
 		int result = preparedStatement.executeUpdate();
+		if (result != 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public Boolean Update(int ID, String password, String name) throws Exception {
+		Connection connection = controller.ConnectDatabase.ConnectMySQLSever();
+		String state = "Update Account Set password=?, name=?, id=?";
+		PreparedStatement preparedStatement = connection.prepareStatement(state);
+		preparedStatement.setString(1, password);
+		preparedStatement.setString(2, name);
+		preparedStatement.setInt(3, ID);
+		int result = preparedStatement.executeUpdate();
+		if (result != 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public Boolean Delete(int ID) throws Exception {
+		Connection connection = controller.ConnectDatabase.ConnectMySQLSever();
+		String state = "Delete From Account Where ID=?";
+		PreparedStatement preparedStatement = connection.prepareStatement(state);
+		preparedStatement.setInt(1, ID);
+		int result = preparedStatement.executeUpdate();
+		if (result != 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public Boolean ChangePassword(int ID, String password) throws Exception {
+		Connection connection = controller.ConnectDatabase.ConnectMySQLSever();
+		String state = "Update Account Set password=? where ID=?";
+		PreparedStatement preparedStatement = connection.prepareStatement(state);
+		preparedStatement.setString(1, password);
+		preparedStatement.setInt(2, ID);
+		int result = preparedStatement.executeUpdate();
 		if(result != 0) {
 			return true;
 		} else return false;
 	}
-	
+
 }
